51d3f6f4baa8999490dcf76d727f8f6798485557
jpier2012 <jpier2012@gmail.com>
2019-07-17 13:02:40 -0700

    complete restaurant creation form, confirm successful creation, params definition, stub show and index pages

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,19 @@
 class RestaurantsController < ApplicationController
     def index
+        @restaurants=Restaurant.all
     end
 
     def new
+        @restaurant = Restaurant.new
     end
 
     def create
+        restaurant = Restaurant.create(restaurant_params)
+        redirect_to restaurant_path(restaurant)
+    end
+
+    def show
+        @restaurant = Restaurant.find_by_id(params[:id])
     end
 
     def edit
@@ -16,4 +24,10 @@ class RestaurantsController < ApplicationController
 
     def delete
     end
+
+    private
+
+        def restaurant_params
+            params.require(:restaurant).permit(:name, :address, :cuisine, :dress_code, :outdoor_seating?, :child_friendly?, :open_bar?, :byob?)
+        end
 end
