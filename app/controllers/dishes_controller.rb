6402b0ac2441fb6f35328382b04fcbab0242bf3f
jpier2012 <jpier2012@gmail.com>
2019-07-17 14:08:25 -0700

    complete new dish form, restaurant collection select, stub dish  new and create controller actions and views

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,9 +3,12 @@ class DishesController < ApplicationController
     end
 
     def new
+        @dish = Dish.new
     end
 
     def create
+        dish = current_user.dishes.create(dish_params)
+        redirect_to dish_path(dish)
     end
 
     def show
@@ -19,4 +22,10 @@ class DishesController < ApplicationController
 
     def delete
     end
+
+    private
+
+        def dish_params
+            params.require(:dish).permit(:name, :description, :price, :taste, :overall_value, :dining_experience, :restaurant_id)
+        end
 end
