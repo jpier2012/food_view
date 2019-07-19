d38cfc288895d9f84f40a241f4426f238ea5e7cc
jpier2012 <jpier2012@gmail.com>
2019-07-19 13:01:01 -0700

    set all as default user cuisine

--- db/migrate/20190719194418_add_filters_to_users.rb
+++ db/migrate/20190719194418_add_filters_to_users.rb
@@ -4,6 +4,6 @@ class AddFiltersToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :outdoor_seating, :boolean
     add_column :users, :family_friendly, :boolean
     add_column :users, :open_bar, :boolean
-    add_column :users, :cuisine, :string
+    add_column :users, :cuisine, :string, default: "all"
   end
 end
