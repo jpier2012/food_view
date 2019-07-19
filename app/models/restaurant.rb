394b97640e2430c71db7e8c112b4c2a9726b7122
jpier2012 <jpier2012@gmail.com>
2019-07-19 08:51:55 -0700

    stub basic cuisine scopes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,9 @@
 class Restaurant < ApplicationRecord
     has_many :dishes
     has_many :users, through: :restaurants
+
+    scope :american, -> { where("lower(cuisine) = ?", "american")}
+    scope :thai, -> { where("lower(cuisine) = ?", "thai")}
+    scope :indian, -> { where("lower(cuisine) = ?", "indian")}
+    scope :chinese, -> { where("lower(cuisine) = ?", "chinese")}
 end
