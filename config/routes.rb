f65bd7dcdeacc5d02c7601d3e3606545f494c20d
jpier2012 <jpier2012@gmail.com>
2019-07-19 11:47:19 -0700

    define filter routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { registrations: "registrations", omniauth_callbacks: "callbacks" }
- 
+
+  get "/filters", to: "users#show_filters", as: "show_filters"
+  post "/filters", to: "users#edit_filters", as: "edit_filters"
+
   resources :dishes
-  resources :restaurants 
+  resources :restaurants
   root to: "application#home"
 
   devise_scope :user do 
