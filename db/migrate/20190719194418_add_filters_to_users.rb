c8fc57f42a5e65c1d795f5003d6133cc7b368c8f
jpier2012 <jpier2012@gmail.com>
2019-07-19 12:49:28 -0700

    edit attribute names

--- db/migrate/20190719194418_add_filters_to_users.rb
+++ db/migrate/20190719194418_add_filters_to_users.rb
@@ -1,9 +1,9 @@
 class AddFiltersToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :byob_filter, :boolean
-    add_column :users, :outdoor_seating_filter, :boolean
-    add_column :users, :family_friendly_filter, :boolean
-    add_column :users, :open_bar_filter, :boolean
-    add_column :users, :cuisine_filter, :string
+    add_column :users, :byob, :boolean
+    add_column :users, :outdoor_seating, :boolean
+    add_column :users, :family_friendly, :boolean
+    add_column :users, :open_bar, :boolean
+    add_column :users, :cuisine, :string
   end
 end
