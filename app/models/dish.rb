d728bdb1c3917355bcf7f8ddff17f748ff0f0e31
jpier2012 <jpier2012@gmail.com>
2019-07-19 11:41:16 -0700

    add basic validations to dish model

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,10 +1,19 @@
 class Dish < ApplicationRecord
     belongs_to :user
     belongs_to :restaurant
-    accepts_nested_attributes_for :restaurant, reject_if: :all_blank
+    accepts_nested_attributes_for :restaurant
+    validates :name, presence: true
+    validates :price, presence: true
+    validates :taste, presence: true
+    validates :overall_value, presence: true
+    validates :dining_experience, presence: true
 
     def restaurant_name
         self.restaurant.name
     end
 
+    def restaurant_attributes=(attributes)
+        restaurant = Restaurant.find_or_create_by(name: attributes[:name])
+        self.restaurant = restaurant
+    end
 end
