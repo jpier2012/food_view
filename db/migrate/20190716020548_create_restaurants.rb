3b623da214f3bf4e6eee9043fe7bbcfe460253f8
jpier2012 <jpier2012@gmail.com>
2019-07-19 06:44:37 -0700

    set boolean field defaults to false

--- db/migrate/20190716020548_create_restaurants.rb
+++ db/migrate/20190716020548_create_restaurants.rb
@@ -5,10 +5,10 @@ class CreateRestaurants < ActiveRecord::Migration[5.2]
       t.string :address
       t.string :cuisine
       t.string :dress_code
-      t.boolean :outdoor_seating, null: false
-      t.boolean :child_friendly, null: false
-      t.boolean :open_bar, null: false
-      t.boolean :byob, null: false
+      t.boolean :outdoor_seating, default: false
+      t.boolean :child_friendly, default: false
+      t.boolean :open_bar, default: false
+      t.boolean :byob, default: false
 
       t.timestamps
     end
