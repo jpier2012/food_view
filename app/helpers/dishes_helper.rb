ae7eeca2a6be7cc985ae0840f98783d23cb58587
jpier2012 <jpier2012@gmail.com>
2019-07-20 21:33:53 -0700

    create access redirect helpers to protect dishes from other users' edits

--- app/helpers/dishes_helper.rb
+++ app/helpers/dishes_helper.rb
@@ -2,4 +2,11 @@ module DishesHelper
     def set_dish
         @dish ||= current_user.dishes.find_by_id(params[:id])
     end
+
+    def dish_access_redirect
+        if @dish.user != current_user
+            current_user.errors[:error] << ": You cannot edit a dish you did not create"
+            render :show
+        end
+    end
 end
--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -3,8 +3,10 @@ module RestaurantsHelper
         @restaurant ||= Restaurant.find_by_id(params[:id])
     end
 
-    def edit_redirect
-        current_user.errors[:error] << ": You cannot edit a Restaurant you did not create"
-        render :show if @restaurant.created_by != current_user.id
+    def restaurant_access_redirect
+        if @restaurant.created_by != current_user.id
+            current_user.errors[:error] << ": You cannot edit a restaurant you did not create"
+            render :show
+        end
     end
 end
