f6d5d62daa92eb54b10ab87d3981d386b397fbd1
James Pier <jpier2012@gmail.com>
2019-07-21 18:41:50 -0500

    update homepage wording

--- app/helpers/dishes_helper.rb
+++ app/helpers/dishes_helper.rb
@@ -3,14 +3,14 @@ module DishesHelper
         @dish ||= Dish.find_by_id(params[:id])
     end
 
-    def set_restaurant
-        @restaurant ||= Restaurant.find_by_id(params[:restaurant_id])
-    end
-
     def dish_access_redirect
         if @dish.user != current_user
             current_user.errors[:error] << ": You cannot edit a dish you did not create"
             render :show
         end
+    end
+
+    def user_owns_dish
+        @dish.user == current_user
     end
 end
