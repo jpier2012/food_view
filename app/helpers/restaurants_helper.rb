594bedbfd72805c4f5729776bb7bb29a3af3157c
jpier2012 <jpier2012@gmail.com>
2019-07-20 20:02:25 -0700

    add edit_redirect restaurant helper

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -2,4 +2,8 @@ module RestaurantsHelper
     def set_restaurant
         @restaurant ||= Restaurant.find_by_id(params[:id])
     end
+
+    def edit_redirect
+        redirect_to restaurants_path if @restaurant.created_by != current_user.id
+    end
 end
