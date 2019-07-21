e3bd5edfcb3adab35d3bbe811bc6177cf1994b01
jpier2012 <jpier2012@gmail.com>
2019-07-20 19:35:09 -0700

    create migration AddCreatedByToRestaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_023112) do
+ActiveRecord::Schema.define(version: 2019_07_21_023124) do
 
   create_table "dishes", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_07_16_023112) do
     t.boolean "byob", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "created_by"
   end
 
   create_table "users", force: :cascade do |t|
